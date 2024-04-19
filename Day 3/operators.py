512495dd42edadf2072b6cb853900e26bc9cbca1
Swapnil Nandalalsing Pardeshi <126460594+swapnil94pardeshi@users.noreply.github.com>
2024-04-19 12:27:31 +0530

    problem statment 5 completed

---
+++ Day 3/operators.py
@@ -0,0 +1,21 @@
+#Declare your age as integer variable
+
+age=int(10)
+
+#Declare your height as a float variable
+height=float(170.5)
+
+#Declare a variable that store a complex number
+complex_number=complex(3,4)
+
+#Write a script that prompts the user to enter base and height of the triangle. calculate an area of this triangle (area = 0.5 x b x h).
+
+base_of_triangle,height_of_triangle=float(input("Enter base of traingle ")),float(input("enter height of triangle 20 "))
+
+area_of_triangle=(0.5*base_of_triangle*height_of_triangle)
+print("Area of Triangle is ", area_of_triangle)
+
+
+#Write a script that prompts the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c).
+
+
