a503d5acad3bf1111839e4b77b2e2f0feb92b0f5
Swapnil Nandalalsing Pardeshi <126460594+swapnil94pardeshi@users.noreply.github.com>
2024-04-19 12:34:54 +0530

    renamed all folders as well as problem statment 6 completed

--- Day_3/operators.py
+++ Day_3/operators.py
@@ -10,7 +10,7 @@ complex_number=complex(3,4)
 
 #Write a script that prompts the user to enter base and height of the triangle. calculate an area of this triangle (area = 0.5 x b x h).
 
-base_of_triangle,height_of_triangle=float(input("Enter base of traingle ")),float(input("enter height of triangle 20 "))
+base_of_triangle,height_of_triangle=float(input("Enter base of traingle ")),float(input("enter height of triangle "))
 
 area_of_triangle=(0.5*base_of_triangle*height_of_triangle)
 print("Area of Triangle is ", area_of_triangle)
@@ -18,4 +18,7 @@ print("Area of Triangle is ", area_of_triangle)
 
 #Write a script that prompts the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c).
 
+side_a,side_b,side_c=float(input("enter side A ")), float(input("enter side B ")), float(input("enter side C "))
 
+perimeter_of_triangle=(side_a+side_b+side_c)
+print("Perimeter of Triangle is ", perimeter_of_triangle)
