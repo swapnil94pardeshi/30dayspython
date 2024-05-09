92bc8d2beae7efb935156c2f65ac95d28eb25519
Swapnil Nandalalsing Pardeshi <126460594+swapnil94pardeshi@users.noreply.github.com>
2024-05-09 15:27:47 +0530

    problme statment 2 solved

--- Day_12/module.py
+++ Day_12/module.py
@@ -9,3 +9,25 @@ def random_user_id():
 print(f"random_user_id is {random_user_id()}")
 
 
+
+import random
+import string
+
+numberofchar=int(input("enter enumber of character"))
+numberofid=int(input("enter number of ids"))
+
+
+def generate_random_user_id():
+
+    random_user_id=[]
+    count = 0
+    # Generate a random six-character user ID
+    while count < numberofid:
+        random_user_id.append(''.join(random.choices(string.ascii_letters + string.digits, k=numberofchar)))
+        count += 1
+    return random_user_id
+
+# Test the function
+print("Random User ID:", generate_random_user_id())
+
+
