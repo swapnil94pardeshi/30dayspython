d2af7958d02585f0f3f9752e7789b78624e6e636
Swapnil Nandalalsing Pardeshi <126460594+swapnil94pardeshi@users.noreply.github.com>
2024-05-15 11:56:56 +0530

    day 14 section 1 solved

--- Day_14/higherorder.py
+++ Day_14/higherorder.py
@@ -17,3 +17,37 @@ for num in numbers:
     print(num)
 
 
+# Use map to create a new list by changing each country to uppercase in the countries list
+
+
+uppercase_letters= map(lambda name:name.upper(),countries)
+
+print(list(uppercase_letters))
+
+# Use map to create a new list by changing each number to its square in the numbers list
+
+squares_numbers= map(lambda num:num*num,numbers)
+print(list(squares_numbers))
+
+#Use map to change each name to uppercase in the names list
+uppercase_name=map(lambda name:name.upper(),names)
+
+print(list(uppercase_name))
+
+#Use filter to filter out countries containing 'land'.
+
+name_in_land= filter(lambda name:"land" in name ,countries)
+
+print(list(name_in_land))
+
+#Use filter to filter out countries having exactly six characters.
+
+name_greater_than_six= filter(lambda name:len(name) == 6,countries)
+
+print(list(name_greater_than_six))
+
+#Use filter to filter out countries containing six letters and more in the country list.
+
+name_greater_than_six_or_more= filter(lambda name:len(name) >= 6,countries)
+
+print(list(name_greater_than_six_or_more))
