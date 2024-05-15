e83672ad49e9f4ca180b765a258f343683cd4728
Swapnil Nandalalsing Pardeshi <126460594+swapnil94pardeshi@users.noreply.github.com>
2024-05-15 11:37:58 +0530

    day 14 added

---
+++ Day_14/higherorder.py
@@ -0,0 +1,19 @@
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#Use for loop to print each country in the countries list.
+
+for country in countries:
+    print(country)
+
+#Use for to print each name in the names list.
+
+for name in names:
+    print(name)
+
+#Use for to print each number in the numbers list.
+for num in numbers:
+    print(num)
+
+
